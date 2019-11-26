c3eabf3e1c4e259d117f8f636d4ecdcfab3baab3
Ruari Day <ruariday@gmail.com>
2019-11-26 17:37:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,6 @@ end
 def using_insert (list_of_programming_languages, another_language )
   new_array = list_of_programming_languages.insert(4,another_language)
 end
-def using_uniq ()
-  
+def using_uniq (haircuts)
+  haircuts.uniq
 end
