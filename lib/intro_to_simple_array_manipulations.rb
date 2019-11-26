ad7150bc499616b3cf40ba7e1ae2d6230219c7ed
Ruari Day <ruariday@gmail.com>
2019-11-26 17:24:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,6 +20,6 @@ end
 def shift_with_args(ice_cream_brands)
   ice_cream_brands.shift(2)
 end
-def using_concat (my_favorite_things, more_faves)
+def using_concat (my_favorite_things, more_favs)
   
 end
