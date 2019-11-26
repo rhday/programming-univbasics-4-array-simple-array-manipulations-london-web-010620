dcfa378eee268bb8deaf688a6c9c278f4d44451c
Ruari Day <ruariday@gmail.com>
2019-11-26 17:09:16 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_push (colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  colors_in_the_rainbow.push(next_color)
+end
+def using_unshift (bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+def using_pop (array)
+ continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"] 
+ continents.pop
+end
+def pop_with_args(array)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  small_dogs = dog_breeds.pop(2)
+end
+
