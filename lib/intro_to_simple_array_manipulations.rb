640679ee5cdd4c447630884ca6c79d7daf0ee581
Ruari Day <ruariday@gmail.com>
2019-11-26 17:34:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,5 +24,5 @@ def using_concat (my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 def using_insert (list_of_programming_languages, another_language )
-  another_language[4].insert(list_of_programming_languages)
+  another_language.insert(list_of_programming_languages)
 end
